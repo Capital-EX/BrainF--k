e9fdb6bbe601b20e92fe5389a1a1b6c048d61793
Capital-EX <xcapt98@gmail.com>
2016-03-28 21:49:45 -0400

    created repository for BrainF--k

---
+++ src/main-obfuscated.c
@@ -0,0 +1,11 @@
+#include<stdio.h>#include<stdlib.h> 
+#define B break
+#define N int
+#define I i++
+#define T (*t)
+#define Q I;B
+#define C(x)case(x+43):if(x!=50&&x!=48&&s){Q;}
+#define F(x,y,z)if(x){y;}else{z;}
+#define W(x)while(x)
+#define R return 0;
+N main(N a,char*v[]){FILE*f;F(a<2,R,f=fopen(v[1],"r");if(!f)R;)N n=64<<9,*t=calloc(n,sizeof(N)),J[n],j=-1,s=0,i=0,l;char p[n];W((l=fgetc(f))!=EOF)p[i++]=l;i=p[i]=0;W(i<n&&p[i]!=0)switch(p[i]){C(19)t++;Q;C(17)t--;Q;C(0)T++;Q;C(2)T--;Q;C(3)printf("%c",T);Q;C(1)T=getchar();Q;C(48)F(T==0,s++;I,J[++j]=i;I)B;C(50)F(T!=0&&!s,i=J[j];j--,F(s!=0,s--,j--);I)B;default:I;}R;}
---
+++ src/main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PROG_SIZE 4096
+#define SKIP if(skip){ip++;break;}
+int main(int argc, char *argv[]){
+    FILE *file;
+    if (argc == 1){
+        printf("No file give...");
+        return 0;
+    }else{
+        file = fopen(argv[1], "r");
+        if (!file){
+            return 0;
+        }
+    }
+    
+    int *tape = calloc(4096, sizeof(int));
+    int tapePointer = 0;
+    
+    int jumpStack[256];
+    int jump = -1;
+    
+    int skip = 0;
+    char prog[PROG_SIZE];
+    int ip = 0;
+    
+    int c;
+    int i = 0;
+    while ( (c = fgetc(file)) != EOF ){
+        prog[i++] = c;
+    }
+    prog[i] = '\0';
+    
+    while ( ip < PROG_SIZE  && prog[ip] != '\0'){
+        switch(prog[ip]){
+            case '>': SKIP
+                tapePointer++;
+                ip++;
+                break;
+
+            case '<': SKIP
+                tapePointer--;
+                ip++;
+                break;
+
+            case '+': SKIP
+                tape[tapePointer]++;
+                ip++;
+                break;
+
+            case '-': SKIP
+                tape[tapePointer]--;
+                ip++;
+                break;
+
+            case '.': SKIP
+                printf("%c",tape[tapePointer]);
+                ip++;
+                break;
+
+            case ',': SKIP
+                tape[tapePointer] = getchar();
+                ip++;
+                break;
+
+            case '[':
+                if (tape[tapePointer] == 0){
+                    skip++;
+                    ip++;
+                }else{
+                    jumpStack[++jump] = ip;
+                    ip++;
+                }
+                break;
+
+            case ']':
+                if (tape[tapePointer] != 0 && !skip){
+                    ip = jumpStack[jump];
+                    jump--;
+                }else{
+                    if (skip != 0){
+                        skip--;
+                    }else{
+                        jump--;
+                    }
+                  
+                    ip++;
+                }
+                 
+                break;
+            default:
+                ip++;
+        }
+    }
+    printf("\n%s\n",prog);
+    return 0;
+}
